9a5debe345c870ef28d6df584572e314eee7cf29
Adam Boyle <adam.boyle@rocksolidinternet.com>
2020-01-15 09:28:01 -0700

    enabled forceIndex parameter for forcing MySQL index in specific reports

--- js/ReportUtils.js
+++ js/ReportUtils.js
@@ -2,129 +2,131 @@ var WebAPI = require('./webAPI');
 //var PivotUtils = require('./PivotUtils');
 
 module.exports = Object.freeze({
+    /* not used? */
+    reportData: function(options, callback) {
+        options.sort = options.sort || {};
 
-	/* not used? */
-	reportData : function(options, callback) {
+        var apiData = this.buildApiData(
+            options.columns,
+            options.rows,
+            options.filters,
+            options.sort,
+            options.top
+        );
 
-		options.sort = options.sort || {};
+        WebAPI.post('report', apiData, function(result) {
+            callback(
+                null,
+                PivotUtils.transform(options.columns, options.rows, result)
+            );
+        });
+    },
 
-		var apiData = this.buildApiData(options.columns, options.rows, options.filters, options.sort, options.top);
+    buildApiData: function(columns, rows, filters, sort, dimensions, top) {
+        /* columns are rows and rows are columns :( */
 
-		WebAPI.post("report", apiData, function(result) {
+        var apiData = {
+            metrics: [],
+            groups: [],
+            filters: [],
+            partitions: [],
+            redshift: 'useredshift' in window ? window.useredshift : true,
+            sort: sort,
+            top: top
+        };
 
-			callback(null, PivotUtils.transform(options.columns, options.rows, result));
+        if (typeof data.forceIndex != 'undefined') {
+            apiData.forceIndex = data.forceIndex;
+        }
 
-		});
+        for (var i = 0; i < columns.length; i++) {
+            var col = columns[i];
+            if (col) {
+                if (dimensions.indexOf(col) != -1) {
+                    //if (col.match && !col.match(/\|count$/) && !col.match(/\|percent/) && !col.match(/\|cumulative/) && (col.match(/^d_/) || col.match(/\.dd/))) {
+                    apiData.partitions.push(columns[i]);
+                } else {
+                    apiData.metrics.push(col);
+                }
+            }
+        }
 
-	},
+        for (var i = 0; i < rows.length; i++) {
+            var col = rows[i];
+            if (col) {
+                if (dimensions.indexOf(col) != -1) {
+                    //if (
+                    //	(col.match && !col.match(/\|count$/) && !col.match(/\|percent/) && !col.match(/\|cumulative/) && (col.match(/^d_/) || col.match(/\.dd/))) ||
+                    //	(col.id && !col.match(/\|count$/) && !col.match(/\|percent/) && !col.match(/\|cumulative/) && col.id.match(/^d/))
+                    //) {
+                    apiData.groups.push(col);
+                } else {
+                    apiData.metrics.push(col);
+                }
+            }
+        }
 
+        //Fixes a bug with mixed objects in an array
+        var metrics = {};
+        apiData.metrics.map(function(element, i) {
+            metrics[i] = element;
+        });
+        apiData.metrics = metrics;
 
-	buildApiData : function(columns, rows, filters, sort, dimensions, top) {
+        //Fixes a bug with mixed objects in an array
+        var groups = {};
+        apiData.groups.map(function(element, i) {
+            groups[i] = element;
+        });
+        apiData.groups = groups;
 
-		/* columns are rows and rows are columns :( */
+        if (window.parent.dashboardFilters) {
+            filters = JSON.parse(JSON.stringify(filters));
+            filters = filters.concat(
+                window.parent.dashboardFilters.filter(dashboardFilter => {
+                    return !filters.some(filter => {
+                        return dashboardFilter.id == filter.id;
+                    });
+                })
+            );
+        }
 
-		var apiData = {
-			metrics : [],
-			groups : [],
-			filters : [],
-			partitions : [],
-			redshift : ('useredshift' in window) ? window.useredshift : true,
-			sort: sort,
-			top: top
-		};
+        for (var i = 0; i < filters.length; i++) {
+            var filter = filters[i];
+            var comparison = filter.comparison;
 
-		for (var i = 0; i < columns.length; i++) {
-			var col = columns[i];
-			if (col) {
-				if (dimensions.indexOf(col) != -1) {
-				//if (col.match && !col.match(/\|count$/) && !col.match(/\|percent/) && !col.match(/\|cumulative/) && (col.match(/^d_/) || col.match(/\.dd/))) {
-					apiData.partitions.push(columns[i]);
-				} else {
-					apiData.metrics.push(col);
-				}
-			}
-		}
+            if (
+                ($.isArray(filter.value) && filter.value.length == 0) ||
+                filter.value == ''
+            ) {
+                filter.value = '';
+            } else {
+                // IF the value is or is not an array, make sure we send the correct var
+                if ($.isArray(filter.value)) {
+                    if (comparison == '=') {
+                        comparison = 'in';
+                    }
+                    if (comparison == '!=') {
+                        comparison = '!in';
+                    }
+                } else {
+                    if (comparison == 'in') {
+                        comparison = '=';
+                    }
+                    if (comparison == '!in') {
+                        comparison = '!=';
+                    }
+                }
 
-		for (var i = 0; i < rows.length; i++) {
-			var col = rows[i];
-			if (col) {
-				if (dimensions.indexOf(col) != -1) {
-				//if (
-				//	(col.match && !col.match(/\|count$/) && !col.match(/\|percent/) && !col.match(/\|cumulative/) && (col.match(/^d_/) || col.match(/\.dd/))) ||
-				//	(col.id && !col.match(/\|count$/) && !col.match(/\|percent/) && !col.match(/\|cumulative/) && col.id.match(/^d/))
-				//) {
-					apiData.groups.push(col);
-				} else {
-					apiData.metrics.push(col);
-				}
-			}
-		}
+                apiData.filters.push({
+                    id: filter.id,
+                    comparison: comparison,
+                    value: filter.value || '',
+                    checkboxes: filter.checkboxes || ''
+                });
+            }
+        }
 
-		//Fixes a bug with mixed objects in an array
-		var metrics = {};
-		apiData.metrics.map(function(element,i) {
-			metrics[i] = element;
-		});
-		apiData.metrics = metrics;
-
-		//Fixes a bug with mixed objects in an array
-		var groups = {};
-		apiData.groups.map(function(element,i) {
-			groups[i] = element;
-		});
-		apiData.groups = groups;
-
-		if (window.parent.dashboardFilters) {
-			filters = JSON.parse(JSON.stringify(filters))
-			filters = filters.concat(
-				window.parent.dashboardFilters.filter((dashboardFilter) => {
-					return !filters.some((filter) => {
-						return dashboardFilter.id == filter.id
-					})
-				})
-			)
-		}
-
-		for (var i = 0; i < filters.length; i++) {
-			var filter = filters[i];
-			var comparison = filter.comparison;
-
-			if (($.isArray(filter.value) && filter.value.length == 0) || filter.value == "") {
-				filter.value = "";
-			} else {
-
-				// IF the value is or is not an array, make sure we send the correct var
-				if ($.isArray(filter.value)) {
-
-					if (comparison == "=") {
-						comparison = "in";
-					}
-					if (comparison == "!=") {
-						comparison = "!in";
-					}
-
-				} else {
-
-					if (comparison == "in") {
-						comparison = "=";
-					}
-					if (comparison == "!in") {
-						comparison = "!=";
-					}
-
-				}
-
-				apiData.filters.push({
-					id : filter.id,
-					comparison : comparison,
-					value : filter.value || '',
-					checkboxes : filter.checkboxes || ''
-				});
-
-			}
-		}
-
-		return apiData;
-	}
-
+        return apiData;
+    }
 });
